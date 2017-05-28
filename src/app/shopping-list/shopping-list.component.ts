1cf59c7a5cc98b32e289fdca98742071968ca885
joshgressman <joshgressman@gmail.com>
2017-05-28 16:36:37 -0500

    added shopping service

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,29 +1,30 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
-
+import {  ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
+
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[];
 
+  constructor(private shoppingListService: ShoppingListService) { }
 
-  ingredients: Ingredient[] = [
-    new Ingredient('Chickern', 5),
-    new Ingredient('olive oil', 1),
-    new Ingredient('Greeen Pepper', 5)
-
-  ];
-  constructor() { }
 
   ngOnInit() {
+  this.ingredients = this.shoppingListService.getIngredients();
+  this.shoppingListService.ingredientsChanged
+  .subscribe(
+    (ingredients: Ingredient[]) => {
+      this.ingredients = ingredients
+    } //listens from the Eventemiter when the ingredients is updated
+  );
   }
 
-  onIngredientAdded(ingredient: Ingredient){
-    this.ingredients.push(ingredient);
-  }
+
 
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,4 +1,25 @@
+import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>() //listens for when the event changes
+  private ingredients: Ingredient[] = [                 //this is how the sliced() array copy gets updated
+    new Ingredient('Chickern', 5),
+    new Ingredient('olive oil', 1),
+    new Ingredient('Greeen Pepper', 5)
 
+  ];
+
+  getIngredients(){
+    return this.ingredients.slice(); //.slice will return a direct copy of the array
+  }
+
+  addIngredient(ingredient: Ingredient){
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 }
+
+
+//Have this service manage the shopping list
+//List of ingredients and the add ingredient method
