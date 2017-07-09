7d68870b0d30b703d63d5add030e97d372103bad
joshgressman <joshgressman@gmail.com>
2017-07-09 11:25:32 -0500

    added auth module and routes

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SignupComponent } from './signup/signup.component';
 import { SigninComponent } from './signin/signin.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
   declarations:[
    SignupComponent,
    SigninComponent
   ],
-  imports:[CommonModule, FormsModule]
+  imports:[CommonModule, FormsModule, AuthRoutingModule]
 })
 
 export class AuthModule {}
